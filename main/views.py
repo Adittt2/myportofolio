4b5fc43c77c6d16e739ac4f969809f216dab87f6
Aditya Hamka Pratama <aditya.hamka@ui.ac.id>
2026-09-09 17:12:54 +0700

    Menyelesaikan seluruh langkah pada tutorial 2 serta telah memastikan bahwa test dapat bekerja dan dapat menangkap kesalahan.

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from main.models import Experience
 
 def show_main(request):
     context = {
-        "name": "Aditya",
+        "name": "Aditya Hamka Pratama",
         "npm": "2506552752",
         "study_program": "S1 Sistem Informasi",
         "bio": (
